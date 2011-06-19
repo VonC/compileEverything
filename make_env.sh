904134ee134f3615064f9e640af9f94b21f099a7
VonC <vonc@laposte.net>
2011-06-19 04:10:16 +0200

    make_env.sh downloads dep file

--- make_env.sh
+++ make_env.sh
@@ -3,3 +3,11 @@ myhome=`pwd`
 echo $myhome
 DIR="$( basename `pwd` )"
 echo $DIR
+d=`date +"%Y%m%d"`
+echo $d
+if [[ ! -e dep ]]; then
+  echo ~~~~ DEPS ~~~~ | tee -a log
+  echo download deps | tee -a log
+  wget http://sunfreeware.com/programlistsparc10.html -O dep$d 2>&1 | tee -a log
+  ln -s dep$d dep 2>&1 | tee -a log
+fi
