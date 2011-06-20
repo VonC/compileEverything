7c83585142a8f4772a2aa7572436b8cfbf3f727a
VonC <vonc@laposte.net>
2011-06-20 04:05:36 +0200

    read _deps, call builds for lib or app

--- make_env.sh
+++ make_env.sh
@@ -21,15 +21,47 @@ function _log() { f=$2; rm -f logs/l; ln -s $f logs/l; _echologcmd "" "$1" $f ; 
 function log() { f=$(_fdate).$2 ; _log "$1" $f ; }
 function loge() { f=$(_fdate).$2 ; echo "(see logs/$f)" >> log; _log "$1" $f ; _echologcmd "DONE ~~~ " "$1" $f; true ; }
 function mrf() { ls -t1 $1 | head -n1 ; }
-#trap "tail -3 log ; f=$(mrf logs) ; tail -5 logs/$f ; exit " EXIT ;
+
 trap "echo -e "\\\\e\\\[00\\\;31m!!!!\nFAIL\n!!!!\\\\e\\\[00m" | tee -a log; tail -3 log ; tail -5 logs/l" EXIT ;
 
-if [[ ! -e dep ]]; then
+if [[ ! -e deps ]]; then
   echolog "#### DEPS ####"
   echolog "download deps from SunFreeware"
   loge "wget http://sunfreeware.com/programlistsparc10.html -O deps$(Ymd)" "wget_deps_sunfreeware"
   log "ln -fs deps$(Ymd) deps" ln_deps
 fi
+function build_app() {
+  local name="$1"
+  echo Building APP $name
+}
+function build_lib() {
+  local name="$1"
+  echo Building LIB $name
+}
+function build_line() {
+  local line="$1"
+  #echo line $line
+  set -- junk $line ; shift
+  local type=$1; local name=$2 ; local deps=$3
+  #echo deps $deps for $name with $type
+  local IFS=”,”; declare -a Array=($deps); local IFS=" "
+  for adep in "${Array[@]}"; do
+    #echo adep $adep for $name with $type
+    if [[ "$adep" != "none" ]]; then
+      adepline=$(grep -E "(app|lib) $adep" _deps)
+      #echo dep line: $adepline
+      build_line "$adepline"
+    fi
+  done
+  #echo done deps from $name with $type
+  if [[ $type == "app" ]]; then build_app "$name" ;
+  elif [[ $type == "lib" ]] ; then build_lib "$name" ; 
+  else echo "unknow type" ; exit 1 ; fi
+}
+cat _deps | while read line; do
+  #echo $line
+  build_line "$line"
+done
 trap - EXIT
 echo -e "\e[00;32mAll Done.\e[00m"
 exit 0
