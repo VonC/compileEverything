f4496bed997d09912b1392c0e54162aa8e8ccf5d
VonC <vonc@laposte.net>
2011-10-10 18:44:26 +0200

    Ubuntu: sshd
    * sshd must be launched with an abolute path
    * usr/local/etc/sshd_config must be amended (port, ...)
    * .ssh/cnf: link to sshd_config
    * bin/sshd script for start/stop/restart/status commands

---
+++ .cpl/scripts/ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p "${H}/.ssh"
+chmod 700 "${H}/.ssh"
+if [[ ! -e "${H}/.ssh/authorized_keys" ]] ; then
+  touch "${H}/.ssh/authorized_keys"
+fi
+chmod 600 "${H}/.ssh/authorized_keys"
+if [[ ! -e "${H}/.ssh/curl-ca-bundle.crt" ]] ; then
+  cp "${H}/.cpl/scripts/curl-ca-bundle.crt" "${H}/.ssh/curl-ca-bundle.crt"
+fi
