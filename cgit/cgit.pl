08db55f02d43233e7d163ce7284afb51105b8352
VonC <vonc@laposte.net>
2011-10-30 22:19:45 +0100

    Cgit is now linked to gitolite
    That means it only displays what a user is authorized to access,
    according to gitolite.
    
     - cgit/cgit.pl: filter lines on cgit main page, check authorizations
     - cgit/error: link to apache/gitcgit_error_log
     - cgit/log: link to apache/gitcgit_access_log
     - gitweb/error: link to apache gitweb_error_log
     - gitweb/log: link to apache gitweb_access_log

--- cgit/cgit.pl
+++ cgit/cgit.pl
@@ -1,7 +1,76 @@
 #!/home/auser/compileEverything/usr/local/apps/perl/bin/perl
-our $s=system("/home/auser/compileEverything/cgit/cgit.cgi");
-our @lines=split($s);
-pop(@lines);
-foreach (@lines) {
-  print $_;
-} 
+#print "<pre>\n";
+use Time::localtime;
+my $remote_user=$ENV{"REMOTE_USER"}; 
+#print "REMOTE_USER = $remote_user<br />";
+my $path_info=$ENV{"PATH_INFO"}; 
+#print "PATH_INFO = $path_info<br />";
+my $request_uri=$ENV{"REQUEST_URI"}; 
+#print "REQUEST_URI = $request_uri<br />";
+# foreach $key (sort keys(%ENV)) {
+#   print "$key = $ENV{$key}<p>";
+# }
+#print "</pre>\n";
+
+my $gl_home = $ENV{HOME} = "/home/auser/compileEverything";
+$ENV{GL_RC} = "$gl_home/.gitolite.rc";
+$ENV{GL_BINDIR} = "$gl_home/gitolite/bin";
+$ENV{GL_USER} = $remote_user;
+# now get gitolite stuff in...
+unshift @INC, $ENV{GL_BINDIR};
+require gitolite_rc;    gitolite_rc -> import;
+require gitolite;       gitolite    -> import;
+# set project root etc. absolute paths
+$ENV{GL_REPO_BASE_ABS} = ( $REPO_BASE =~ m(^/) ? $REPO_BASE : "$gl_home/$REPO_BASE" );
+$projects_list = $projectroot = $ENV{GL_REPO_BASE_ABS};
+#print "ok: projects_list $projects_list<br />";
+
+if ($request_uri ne "/cgit/" && $request_uri ne "/cgit/cgit.pl/") {
+  (my $repo)=($path_info =~ /\/([^\/]+)/);
+  #print "ok: repo $repo<br />";
+  my ($perm, $creator) = &repo_rights($repo);
+  #print "ok: perm $perm<br />";
+  #print "ok: creator $creator<br />";
+  if ($perm =~ /R/) {
+    system("/home/auser/compileEverything/cgit/cgit.cgi");
+  }
+  else {
+    print "Content-type: text/html\n\n";
+    print "<html>\n";
+    print "<body>\n";
+    print "	<h1>HTTP Status 403 - Access is denied</h1>\n";
+    print "	<h3>You don't have access to repo $repo as $remote_user</h3>\n";
+    print "</body>\n";
+    print "</html>\n";
+  }
+}
+else {
+    my $fname="$remote_user.".timestamp().".tpl";
+    system("/home/auser/compileEverything/cgit/cgit.cgi > $fname");
+    open(INFO, $fname);		# Open the file
+    @lines = <INFO>;		# Read it into an array
+    close(INFO);
+    unlink($fname);
+    pop(@lines);
+    foreach (@lines) {
+      my $line=$_;
+      (my $repo)=($line =~ /title='([^']+)'/);
+      my $perm = "R";
+      if ($repo ne "") {
+        my ($aperm, $creator) = &repo_rights($repo);
+        $perm=$aperm;
+      }
+      if ($perm =~ /R/) {
+        print $line;
+      }
+    }
+}
+
+sub timestamp {
+  my $t = localtime;
+  return sprintf( "%04d-%02d-%02d_%02d-%02d-%02d",
+                  $t->year + 1900, $t->mon + 1, $t->mday,
+                  $t->hour, $t->min, $t->sec );
+}
+
+
