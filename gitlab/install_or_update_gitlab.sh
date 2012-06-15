d147ee6bc42ec64a99a06cba0448acfc9a17fb55
VonC <vonc@laposte.net>
2012-06-15 23:48:22 +0200

    Ubuntu: mysql and gitlab fixes
    .cpl/params/mysql:
      add '-DCMAKE_SKIP_RPATH=ON' to avoid aving runtime path stripped
      during the installation step
    .cpl/params/gitlab:
      Re-activate gitlab installation script (uncommented)
    .cpl/params/mysql:
      make the '@@VER@@' symlink in '$H/mysql' to mysql installation path
    .gitignore:
      ignore /mysql/5.5.25
    gitlab/install_or_update_gitlab.sh:
      execute the 'bundle config' in the right gitlab directory

--- gitlab/install_or_update_gitlab.sh
+++ gitlab/install_or_update_gitlab.sh
@@ -6,9 +6,12 @@ mysqlgtl="${H}/mysql/sandboxes/gitlab"
 
 if [[ ! -e "${github}" ]] ; then
   xxgit=1 git clone https://github.com/gitlabhq/gitlabhq "${github}"
+  d=$(pwd)
+  cd "${github}"
   bundle config build.charlock_holmes --with-icu-dir="${HUL}"
   bundle config build.raindrops --with-atomic_ops-dir="${HUL}"
   bundle config build.sqlite3 --with-sqlite3-dir="${HUL}"
+  cd "${d}"
 else
   xxgit=1 git --work-tree="${github}" --git-dir="${github}/.git" pull
 fi
@@ -16,7 +19,7 @@ cp_tpl "${gtl}/gitlab.yml.tpl" "${gtl}"
 cp_tpl "${gtl}/database.yml.tpl" "${gtl}"
 ln -fs ../../gitlab.yml "${github}/gitlab.yml"
 ln -fs ../../database.yml "${github}/database.yml"
-if [[ !- "${mysqlgtl}" ]] ; then
-  mysqlv=$(mysql -V); =${mysqlv%%,*} ; =${mysqlv##* }
-  make_sandbox ${mysqlv} -- -d "${mysqlgtl}"
+if [[ ! -e "${mysqlgtl}" ]] ; then
+  mysqlv=$(mysql -V); mysqlv=${mysqlv%%,*} ; mysqlv=${mysqlv##* }
+  make_sandbox ${mysqlv} -- -d gitlab 
 fi
