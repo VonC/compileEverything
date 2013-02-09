209055683361e80ec47f13ce587d0748326d3c2c
VonC <vonc@laposte.net>
2013-02-09 23:30:42 +0100

    Ubuntu Mint 64 gcc ld compilation issue

---
+++ .cpl/tests/groff/conftest.c
@@ -0,0 +1,29 @@
+/* confdefs.h */
+#define PACKAGE_NAME "groff"
+#define PACKAGE_TARNAME "groff-1.22.2"
+#define PACKAGE_VERSION "1.22.2"
+#define PACKAGE_STRING "groff 1.22.2"
+#define PACKAGE_BUGREPORT "bug-groff@gnu.org"
+#define PACKAGE_URL ""
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define __EXTENSIONS__ 1
+#define _ALL_SOURCE 1
+#define _GNU_SOURCE 1
+#define _POSIX_PTHREAD_SEMANTICS 1
+#define _TANDEM_SOURCE 1
+/* end confdefs.h.  */
+
+
+
+int main() {
+  return 0;
+}
