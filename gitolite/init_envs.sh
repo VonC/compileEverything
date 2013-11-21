2541a8131cccee470ee637ab7b6839ca78355259
VonC <vonc@laposte.net>
2013-11-21 10:36:03 +0000

    Fix gitolite/init_envs.sh to add proper remote url to gitolite-admin.

--- gitolite/init_envs.sh
+++ gitolite/init_envs.sh
@@ -9,12 +9,38 @@ get_tpl_value "${H}/.envs.private" "@UPSTREAM_NAME@" upstream_name
 
 if [[ "${upstream_url}" == "" || "${upstream_name}" == "" ]] ; then exit 0 ; fi
 
+# A login must be defined for pushing gitolite admin repo
+get_tpl_value "${H}/.envs.private" "@USER_GA_PUSH@" user_ga_push
+if [[ "${user_ga_push}" == "" ]] ; then
+  echo "No user is registered to push gitolite-admin to upstream url '${upstream_url}'"
+  exit 0
+fi
+
+upstream_url="https://${user_ga_push}@${upstream_url#https://}"
+
+GIT_DIR="${H}/repositories/gitolite-admin.git"
+xxgit=1
+
+r=$(git remote show -n ${upstream_name}|grep "https")
+
+if [[ "${r}" == "" ]] ; then
+  echo "register '${upstream_name}' as '${upstream_url}gitolite-admin'"
+  git remote add ${upstream_name} ${upstream_url}gitolite-admin
+fi
+
+r=$(git remote show -n ${upstream_name}|grep "https"|grep "${user_ga_push}@")
+
+if [[ "${r}" == "" ]] ; then
+  echo "update '${upstream_name}' as '${upstream_url}gitolite-admin'"
+  git remote set-url ${upstream_name} ${upstream_url}gitolite-admin
+fi
+
+if [[ -e "${H}/.gnupg/users.netrc.asc" ]]; then
+  chelper=$(git config --local --get credential.helper)
+  if [[ "${chelper#netrc }" == "${chelper}" ]] ; then
+    git config --local credential.helper 'netrc -f ${H}/.gnupg/users.netrc.asc -v -d'
+  fi
+fi
+
 gtl="${H}/gitolite"
 ln -fs "../../../gitolite/post-receive-gitolite-admin" "${H}/repositories/gitolite-admin.git/hooks/post-receive"
-
-r=$(GIT_DIR="${H}/repositories/gitolite-admin.git" xxgit=1 git remote show -n ${upstream_name}|grep "https")
-if [[ "${r}" == "" ]] ; then
-  GIT_DIR="${H}/repositories/gitolite-admin.git" xxgit=1 git remote add ${upstream_name} ${upstream_url}gitolite-admin
-else
-  GIT_DIR="${H}/repositories/gitolite-admin.git" xxgit=1 git remote set-url ${upstream_name} ${upstream_url}gitolite-admin
-fi
